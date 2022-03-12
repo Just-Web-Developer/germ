ff4254de3daea7824bcc47ce9e03c19c5f054157
Vladyslav Smolianets <74509296+Just-Web-Developer@users.noreply.github.com>
2022-03-12 16:02:43 +0200

    init

---
+++ components/Galery.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>dgfdg</div>
+</template>
+<script>
+export default {
+  name:"GaleryComponent"
+}
+</script>
---
+++ components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="w-screen h-16 flex justify-between border-b-2 border-blue-200">
+    <div class="logo">
+
+    </div>
+    <div class="langs">
+
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent',
+}
+</script>
